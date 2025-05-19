5062def798a71802bffe3e158d36a27c0818be30
Jaiden <jmt268@byu.edu>
2025-05-19 14:59:00 -0600

    Working on phase 3 - created DataAccess.java and MemoryDataAccess.java

---
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -0,0 +1,91 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemoryDataAccess implements DataAccess {
+    private final Map<String, UserData> users = new HashMap<>();
+    private final Map<String, GameData> games = new HashMap<>();
+    private final Map<String, AuthData> auths = new HashMap<>();
+
+    //clear all data
+    @Override
+    public void clear() throws DataAccessException {
+        users.clear();
+        games.clear();
+        auths.clear();
+    }
+
+    //user methods
+    @Override
+    public void insertUser(UserData user) throws DataAccessException {
+        if (user == null || user.username() == null) {
+            throw new DataAccessException("User or username is null");
+        }
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("User already exists");
+        }
+        users.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return users.get(username);
+    }
+
+    //game methods
+    @Override
+    public void insertGame(GameData game) throws DataAccessException {
+        if (game == null) {
+            throw new DataAccessException("Game is null");
+        }
+        if (games.containsKey(game.gameID())) {
+            throw new DataAccessException("Game already exists");
+        }
+        games.put(String.valueOf(game.gameID()), game);
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return games.get(gameID);
+    }
+
+    @Override
+    public List<GameData> listGames() throws DataAccessException {
+        return new ArrayList<>(games.values());
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+        if (game == null || !games.containsKey(game.gameID())) {
+            throw new DataAccessException("Game does not exist.");
+        }
+        games.put(String.valueOf(game.gameID()), game);
+    }
+
+    //auth methods
+    @Override
+    public void insertAuth(AuthData auth) throws DataAccessException {
+        if (auth == null || auth.authToken() == null) {
+            throw new DataAccessException("Auth or token is null");
+        }
+        auths.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return auths.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        auths.remove(authToken);
+    }
+}
