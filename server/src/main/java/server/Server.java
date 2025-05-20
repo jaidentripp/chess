4ec7509580ee85110f7158659aad201bf0e5846d
Jaiden <jmt268@byu.edu>
2025-05-20 17:19:40 -0600

    Working on phase 3 - clear endpoint

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,17 +2,48 @@ package server;
 
 import spark.*;
 
+import com.google.gson.Gson;
+import dataaccess.DataAccess;
+import dataaccess.MemoryDataAccess;
+import service.ClearService;
+import dataaccess.DataAccessException;
+import java.util.Map;
+
 public class Server {
+
+//    public int run(int desiredPort) {
+//        Spark.port(desiredPort);
+//
+//        Spark.staticFiles.location("web");
+//
+//        // Register your endpoints and handle exceptions here.
+//
+//        //This line initializes the server and can be removed once you have a functioning endpoint
+//        Spark.init();
+//
+//        Spark.awaitInitialization();
+//        return Spark.port();
+//    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
+        Spark.staticFileLocation("web");
 
-        Spark.staticFiles.location("web");
+        DataAccess dao = new MemoryDataAccess();
+        Gson gson = new Gson();
 
-        // Register your endpoints and handle exceptions here.
-
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        //Register endpoints
+        Spark.delete("/db", (req, res) -> {
+            try {
+                new ClearService(dao).clear();
+                res.status(200);
+                res.type("application/json");
+                return "{}";
+            } catch (DataAccessException e) {
+                res.status(500);
+                return gson.toJson(Map.of("message", "Error: " + e.getMessage()));
+            }
+        });
 
         Spark.awaitInitialization();
         return Spark.port();
