c860c5be9a6495143d7e8b5e802f9a9f88ec16f7
Jaiden <jmt268@byu.edu>
2025-05-20 15:47:27 -0600

    Working on phase 3 - GameService, createGame request/result and join game/list games

---
+++ server/src/main/java/result/ListGamesResult.java
@@ -0,0 +1,7 @@
+package result;
+
+import model.GameData;
+
+import java.util.List;
+
+public record ListGamesResult(List<GameData> games) {}
---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,65 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.*;
+import request.*;
+import result.*;
+import java.util.*;
+
+public class GameService {
+    private final DataAccess dao;
+    private static int nextGameId = 1;
+
+    public GameService(DataAccess dao) { this.dao = dao; }
+
+    public CreateGameResult createGameResult(CreateGameRequest req, String authToken) throws DataAccessException {
+        AuthData auth = dao.getAuth(authToken);
+        if (auth == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+        if (req == null || req.gameName() == null || req.gameName().isBlank()) {
+            throw new DataAccessException("Error: bad request");
+        }
+        int gameID = nextGameId++;
+        GameData game = new GameData(gameID, null, null, req.gameName(), new chess.ChessGame());
+        dao.insertGame(game);
+        return new CreateGameResult(gameID);
+    }
+
+    public ListGamesResult listGames(String authToken) throws DataAccessException {
+        AuthData auth = dao.getAuth(authToken);
+        if (auth == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+        List<GameData> games = dao.listGames();
+        return new ListGamesResult(games);
+    }
+
+    public void joinGame(JoinGameRequest req, String authToken) throws DataAccessException {
+        AuthData auth = dao.getAuth(authToken);
+        if (auth == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+        GameData game = dao.getGame(req.gameID());
+        if (game == null) {
+            throw new DataAccessException("Error: bad request");
+        }
+
+        //assign player to color
+        if ("WHITE".equalsIgnoreCase(req.playerColor())) {
+            if (game.whiteUsername() != null) {
+                throw new DataAccessException("Error: already taken");
+            }
+            game = new GameData(game.gameID(), auth.username(), game.blackUsername(), game.gameName(), game.game());
+        } else if ("BLACK".equalsIgnoreCase(req.playerColor())) {
+            if (game.blackUsername() != null) {
+                throw new DataAccessException("Error: already taken");
+            }
+            game = new GameData(game.gameID(), game.whiteUsername(), auth.username(), game.gameName(), game.game());
+        } else {
+            throw new DataAccessException("Error: bad request");
+        }
+        dao.updateGame(game);
+    }
+}
