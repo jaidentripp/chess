06193c3497a3e0afc9f3ca48fcaf7c377a32e805
Jaiden <jmt268@byu.edu>
2025-05-21 15:00:29 -0600

    Working on phase 3 - added create game and list games endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,6 +3,7 @@ package server;
 import com.google.gson.JsonSyntaxException;
 import request.LoginRequest;
 import request.RegisterRequest;
+import result.ListGamesResult;
 import result.LoginResult;
 import result.RegisterResult;
 import service.UserService;
@@ -14,6 +15,11 @@ import dataaccess.MemoryDataAccess;
 import service.ClearService;
 import dataaccess.DataAccessException;
 import java.util.Map;
+
+import service.GameService;
+import result.ListGamesResult;
+import request.CreateGameRequest;
+import result.CreateGameResult;
 
 public class Server {
 
@@ -39,6 +45,7 @@ public class Server {
         Gson gson = new Gson();
         ClearService clearService = new ClearService(dao);
         UserService userService = new UserService(dao);
+        GameService gameService = new GameService(dao);
 
         //Clear endpoint
         Spark.delete("/db", (req, res) -> {
@@ -109,6 +116,51 @@ public class Server {
             } catch (DataAccessException e) {
                 res.status(401);
                 return gson.toJson(Map.of("message", e.getMessage()));
+            } catch (Exception e) {
+                res.status(500);
+                return gson.toJson(Map.of("message", "Error: " + e.getMessage()));
+            }
+        });
+
+        //List games endpoint
+        Spark.get("/game", (req, res) -> {
+            try {
+                String authToken = req.headers("authorization");
+                ListGamesResult result = gameService.listGames(authToken);
+                res.type("application/json");
+                res.status(200);
+                return gson.toJson(result);
+            } catch (DataAccessException e) {
+                res.status(401);
+                return gson.toJson(Map.of("message", e.getMessage()));
+            } catch (Exception e) {
+                res.status(500);
+                return gson.toJson(Map.of("message", "Error: " + e.getMessage()));
+            }
+        });
+
+        //Create game endpoint
+        Spark.post("/game", (req, res) -> {
+            try {
+                String authToken = req.headers("authorization");
+                CreateGameRequest request = gson.fromJson(req.body(), CreateGameRequest.class);
+                CreateGameResult result = gameService.createGameResult(request, authToken);
+                res.type("application/json");
+                res.status(200);
+                return gson.toJson(result);
+            } catch (com.google.gson.JsonSyntaxException e) {
+                res.status(400);
+                return gson.toJson(Map.of("message", "Error: bad request"));
+            } catch (dataaccess.DataAccessException e) {
+                String message = e.getMessage();
+                if ("Error: bad request".equals(message)) {
+                    res.status(400);
+                } else if ("Error: unauthorized".equals(message)) {
+                    res.status(401);
+                } else {
+                    res.status(500);
+                }
+                return gson.toJson(Map.of("message", message));
             } catch (Exception e) {
                 res.status(500);
                 return gson.toJson(Map.of("message", "Error: " + e.getMessage()));
