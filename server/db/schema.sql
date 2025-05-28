9ca693e4df4402dd059bf83b43ba04bcec5cbd8d
Jaiden <jmt268@byu.edu>
2025-05-28 14:28:39 -0600

    Working on phase 4 - updated schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -5,21 +5,21 @@ USE chess;
 CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(255) PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
+    email VARCHAR(255) NOT NULL UNIQUE
     );
 
 CREATE TABLE IF NOT EXISTS auths (
     authToken VARCHAR(255) PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
-    FOREIGN KEY (username) REFERENCES users(username)
+    FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE
     );
 
 CREATE TABLE IF NOT EXISTS games (
-                                     gameID INT AUTO_INCREMENT PRIMARY KEY,
-                                     whiteUsername VARCHAR(255),
+    gameID INT AUTO_INCREMENT PRIMARY KEY,
+    whiteUsername VARCHAR(255),
     blackUsername VARCHAR(255),
     gameName VARCHAR(255) NOT NULL,
     game TEXT NOT NULL,
-    FOREIGN KEY (whiteUsername) REFERENCES users(username),
-    FOREIGN KEY (blackUsername) REFERENCES users(username)
+    FOREIGN KEY (whiteUsername) REFERENCES users(username) ON DELETE SET NULL,
+    FOREIGN KEY (blackUsername) REFERENCES users(username) ON DELETE SET NULL
     );
