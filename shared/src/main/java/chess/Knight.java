b167542d38e69c523985d0dda6a65773bf72951a
Jaiden <jmt268@byu.edu>
2025-06-11 13:05:51 -0600

    Working on phase 6 - unused code in Knight and King

--- shared/src/main/java/chess/Knight.java
+++ shared/src/main/java/chess/Knight.java
@@ -25,19 +25,5 @@ public class Knight {
         KingKnightShift.shift(board, myPosition, moves, 1, 2, this.color);
         return moves;
     }
-
-//    private void shift(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves, int cDirection, int rDirection) {
-//        int c = myPosition.getColumn() + cDirection;
-//        int r = myPosition.getRow() + rDirection;
-//        if (c > 0 && c < 9 && r > 0 && r < 9) {
-//            ChessPosition pos = new ChessPosition(r, c);
-//            if (board.getPiece(pos) == null) {
-//                moves.add(new ChessMove((ChessPosition) myPosition, pos, (ChessPiece.PieceType)null));
-//            } else if (board.getPiece(pos).getTeamColor() != this.color) {
-//                moves.add(new ChessMove((ChessPosition) myPosition, pos, (ChessPiece.PieceType)null));
-//            }
-//        }
-//
-//    }
 }
 
